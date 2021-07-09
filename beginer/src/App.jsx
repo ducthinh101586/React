5bfdc36e3ad651926466544aa3a5ced570240406
DucThinh <ngoducthinh586@gmail.com>
2021-07-09 17:20:07 +0700

    tao Route cho cac page

--- beginer/src/App.jsx
+++ beginer/src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import Register from './pages/Register'
+import StudentManagement from './pages/StudentManagement'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route path="/register" component={BrowserRouter} />
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/student" component={StudentManagement} />
       </BrowserRouter>
-      <Register />
     </div>
   )
 }
